7aee0c84e93f0ea5fff2b9a30b3da723ff7f62b7
MarcoLuna <lunagonzalesmarco@gmail.com>
2024-05-16 10:04:37 -0400

    model providers

---
+++ database/migrations/2024_05_16_133307_create_providers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProvidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('providers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('address');
+            $table->string('email');
+            $table->string('phone_number');
+            $table->string('nit');
+            $table->integer('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('providers');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         //$this->call(CategoryTableSeeder::class);
-        \App\Models\Category::factory(20)->create();
+        \App\Models\Category::factory(10)->create();
+        \App\Models\Provider::factory(30)->create();
     }
 }
